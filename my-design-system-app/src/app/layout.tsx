5f033f4f7b00e360379565a0bc7afc132f07176c
vincentlow02 <226811304+vincentlow02@users.noreply.github.com>
2026-03-28 19:35:04 +0900

    Add Microsoft Clarity tracking tracking script

--- my-design-system-app/src/app/layout.tsx
+++ my-design-system-app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
+      <head>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w2s0mqamck");
+          `}
+        </Script>
+      </head>
       <body className="min-h-full">{children}</body>
     </html>
   );
